6d4b63ac28ee7074458320d08568a267cf998580
Luke1024 <chajdas.lukasz@gmail.com>
2022-01-11 20:55:00 +0100

    repositioning

--- developer-page/src/app/card-block/card-block.component.ts
+++ developer-page/src/app/card-block/card-block.component.ts
@@ -10,6 +10,7 @@ import { Card } from '../models/card';
 export class CardBlockComponent implements OnInit {
 
   cards:Card[] = []
+  cards2:Card[] = []
   cardModal:Card = {} as Card;
 
   //modal Urls
@@ -24,7 +25,7 @@ export class CardBlockComponent implements OnInit {
 
   ngOnInit(): void {
     //this.messageService.getToken()
-    this.cards.push({
+    let card = {
       title:"TodoApp",
       tech:"Angular/Spring Boot",
       imgUrl:"assets/images/img1.png",
@@ -32,7 +33,10 @@ export class CardBlockComponent implements OnInit {
       description:"This is the simplest application that I was able to use to learn proper full-stack development. When I started this app I knew nothing about front-end development so started from vanilla js. I quickly learned that programming client with vanilla js is quite pointless and moved to jquery. Jquery code quickly became quite chaotic. I was aware then that newer frameworks structure code better so I moved to Angular with typescript as the ultimate solution. This was an interesting course in the history of evolution in front-end development for me.",
       appUrl:"https://morning-coast-72770.herokuapp.com/",
       repoUrl:"https://github.com/Luke1024/ToDo_fullStackApp"
-    } as Card);
+    } as Card;
+
+    this.cards.push(card,card,card);
+    this.cards2.push(card);
   }
 
   send(code:string){
